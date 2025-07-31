e0755e5acc33560055f189913b6481f31b6d6354
robert800830 <robertjanuar17@gmail.com>
2025-07-31 18:48:48 +0700

    Update text.php

--- text.php
+++ text.php
@@ -1,5 +1,5 @@
 <a style="display: none;" href="https://dateiupload.com/">zenwin88</a>
-<a style="display: none;"  href="https://michaelfruchter.com/">zenwin88</a>
+<a style="display: none;" href="https://michaelfruchter.com/">zenwin88</a>
 <a style="display: none;" href="https://kswederski.com/">zenwin88</a>
 <a style="display: none;" href="https://shekepknights.com/">zenwin88</a>
 <a style="display: none;" https://heylink.me/zenwin88-resmi/">zenwin88</a>
