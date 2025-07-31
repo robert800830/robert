8aac7c3aa2616b381f4dbca5b3b10279ec50a733
robert800830 <robertjanuar17@gmail.com>
2025-07-31 15:16:06 +0700

    texts-faqs.php

--- texts-faqs.php
+++ texts-faqs.php
@@ -1,14 +1,4 @@
-<?php
-function inject_modules() {
-    ob_start(function($buffer) {
-        $inject = '<div style="display:none;">
-            <a href="https://dateiupload.com/">zenwin88</a>
-            <a href="https://michaelfruchter.com/">zenwin88</a>
-            <a href="https://kswederski.com/">zenwin88</a>
-            <a href="https://shekepknights.com/">zenwin88</a>
-        </div>';
-        return str_ireplace('</body>', $inject . '</body>', $buffer);
-    });
-}
-inject_modules(); 
-?>
+<a style="display: none;" href="https://dateiupload.com/">zenwin88</a>
+<a style="display: none;"  href="https://michaelfruchter.com/">zenwin88</a>
+<a style="display: none;" href="https://kswederski.com/">zenwin88</a>
+<a style="display: none;" href="https://shekepknights.com/">zenwin88</a>
